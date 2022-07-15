3fe652d0c05badc7155f84b0899e6772435d1c0d
Anna-Bekreneva <anjabekreneva@gmail.com>
2022-07-15 19:45:51 +0300

    countPositivesSumNegatives

---
+++ 8/countPositivesSumNegatives.js
@@ -0,0 +1,31 @@
+function countPositivesSumNegatives(input) {
+    if (input == null || input.length == 0) {
+        const emptyArray = []
+        return emptyArray
+    } else {
+        const arrayNegative = []
+        const arrayPositive = []
+
+        input.forEach(element => {
+            if (element < 0) {
+                let negative = element
+                arrayNegative.unshift(negative)
+            }
+            if (element > 0) {
+                let positive = element
+                arrayPositive.unshift(positive)
+            }
+        });
+
+        const sumNegatives = arrayNegative.reduce((sum, current) => {
+            return sum + current;
+        })
+
+        const countPositives = arrayPositive.length
+
+        const arrayResult = [countPositives, sumNegatives]
+        return arrayResult
+    }
+}
+
+console.log(countPositivesSumNegatives([]))
