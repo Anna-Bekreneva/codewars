1b9a91abbc02ecc8e62cf49c5790099ba63765e2
Anna-Bekreneva <anjabekreneva@gmail.com>
2022-07-18 19:18:41 +0300

    added amIWilson

---
+++ 8/amIWilson.js
@@ -0,0 +1,11 @@
+function amIWilson(p) {
+    if (p === 5 || p === 13 || p === 563) {
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(amIWilson(136))
+
+// 5, 13, 563
