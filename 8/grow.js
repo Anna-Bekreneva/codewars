3765073219205c017cbc480759593060f78fc6ad
Anna-Bekreneva <anjabekreneva@gmail.com>
2022-12-27 18:49:37 +0300

    add task shortcut

--- 8/grow.js
+++ 8/grow.js
@@ -1,3 +1,1 @@
 const grow = (x) => x.reduce((a, c) => a * c)
-
-console.log(grow([1,2,3]))
---
+++ 8/shortcut.js
@@ -0,0 +1,18 @@
+function shortcut (string) {
+    const arrayLetters = ['a', 'e', 'i', 'o', 'u']
+    let newString = string
+
+    for (let i = 0; i < arrayLetters.length; i++) {
+        if (string.includes(arrayLetters[i])) {
+            const element = arrayLetters[i]
+            newString = newString.replaceAll(arrayLetters[i], "")
+        }
+    }
+    return newString
+}
+console.log(shortcut('how are you today?'))
+
+function shortcut2(string){
+    return string.replace(/[aeiou]/gi, '');
+}
+console.log(shortcut2('how are you today?'))
